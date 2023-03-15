121f551493d791d82b81a66cb975c2a903509654
kwonchanwooo <rlaalfk121@gmail.com>
2023-03-15 11:32:38 +0900

    배경 그라데이션

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { FaBell, FaSearch } from 'react-icons/fa';
 
 function Header(props: any) {
+	const [Scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => (window.scrollY > 0 ? setScrolled(true) : setScrolled(false));
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<header>
-			<div>
+		<header className={`transition-colors duration-[.5s] ${Scrolled && 'bg-[#141414]'}`}>
+			<div className='flex items-center space-x-2 md:space-x-10'>
 				<h1>
 					<img src='https://rb.gy/ulxxee' alt='netflix' width={100} className='cursor-pointer' />
 				</h1>
 
-				<ul className='space-x-4 flex md:flex'>
+				<ul className='space-x-4 hidden md:flex'>
 					<li className='headerLink'>Home</li>
 					<li className='headerLink'>TV SHOW</li>
 					<li className='headerLink'>Movies</li>
@@ -18,14 +27,13 @@ function Header(props: any) {
 				</ul>
 			</div>
 
-			<div>
-				<FaSearch>
-					<p>Kids</p>
-				</FaSearch>
+			<div className='flex items-center space-x-4 text-sm font-light'>
+				<FaSearch className='w-6' />
+				<p className='hidden lg:inline'>Kids</p>
+				<FaBell className='w-6' />
 				<Link href='/'>
 					<img src='https://rb.gy/g1pwyx' alt='profile' className='rounded' />
 				</Link>
-				<FaBell></FaBell>
 			</div>
 		</header>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,14 @@ import Head from 'next/head';
 
 const Home: NextPage = () => {
 	return (
-		<div>
+		<div className='relative h-screen bg-gradient-to-b from-[#333] to-[#141414]'>
 			<Head>
 				<title>HELLOFLIX</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Header />
 			<main>
-				<h1></h1>
+				<section></section>
 			</main>
 		</div>
 	);
